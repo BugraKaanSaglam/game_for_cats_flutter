30d683c6409627f84aa32ed2f62ad551127161d9
BugraKaanSaglam <bugrakaansaglam@gmail.com>
2024-08-23 15:08:35 +0300

    flutter patch

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, use_build_context_synchronously
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:game_for_cats_flutter/database/db_error.dart';
